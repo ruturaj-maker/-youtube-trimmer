12b4fcb1b24450e8004dd7820afc86fb167d9a49
ruturaj-maker <ruturajkulkarni60@gmail.com>
2025-07-16 23:31:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,4 +35,5 @@ def trim():
     return send_file(out_file, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+  app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
